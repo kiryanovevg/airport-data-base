4cb2af159850370a2f2a41c80d706398e36aef56
Evgeniy Kiryanov <evgeniy.kiryanov@roonyx.team>
2019-04-30 21:26:26 +0300

    adding webpack

--- src/main/kotlin/com/kiryanov/database/config/MvcConfig.kt
+++ src/main/kotlin/com/kiryanov/database/config/MvcConfig.kt
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 
-@Configuration
+//@Configuration
 class MvcConfig : WebMvcConfigurerAdapter() {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/**")
---
+++ src/main/resources/static/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Welcome</h1>
+        <div>{{ msg }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
