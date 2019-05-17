7952e719929e8258ce9af0ca25c8f210cb90ca46
Evgeniy Kiryanov <evgeniy.kiryanov@roonyx.team>
2019-05-17 19:48:41 +0300

    List

--- src/main/resources/static/js/components/List.vue
+++ src/main/resources/static/js/components/List.vue
@@ -4,18 +4,27 @@
 
         <app-loading :loading="loading"/>
 
-        <ul class="list-group scrollable"
-            v-if="!loading"
-        >
-            <li
-                    class="list-group-item list-group-item-action"
-                    v-for="(item, index) in items"
-                    v-on:click="select(index)"
-                    v-bind:class="{ active: item === selected }"
-            >
-                {{index + 1}} - {{ fill(item) }}
-            </li>
-        </ul>
+        <div v-if="!loading">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2"
+                       v-model="search"
+                >
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button" v-on:click="clear">Clear</button>
+                </div>
+            </div>
+
+            <ul class="list-group scrollable">
+                <li
+                        class="list-group-item list-group-item-action"
+                        v-for="(item, index) in filtered"
+                        v-on:click="select(index)"
+                        v-bind:class="{ active: item === selected }"
+                >
+                    {{items.indexOf(item) + 1}} - {{ fill(item) }}
+                </li>
+            </ul>
+        </div>
 
         <hr>
     </div>
@@ -34,6 +43,7 @@
         data() {
             return {
                 selected: null,
+                search: null,
             }
         },
         watch: {
@@ -41,11 +51,45 @@
                 this.selected = this.value
             }
         },
+        computed: {
+            filtered() {
+                const items = this.items;
+                const search = this.search;
+
+                if (items){
+                    if (!search) return this.items;
+                    return items.filter(value => {
+                        return contains(value, search);
+                    });
+                }
+            }
+        },
         methods: {
             select(index) {
-                this.$emit('input', this.items[index])
+                this.$emit('input', this.filtered[index])
+            },
+
+            clear() {
+                this.search = null;
             }
         }
+    }
+
+    function contains(value, search) {
+        let result = false;
+        Object.keys(value).forEach(key => {
+            if (value[key] != null) {
+                if (value[key] instanceof Date) {
+
+                } else if (value[key] instanceof Object) {
+                    if (!result) result = contains(value[key], search);
+                } else if (!result) result = value[key]
+                    .toString()
+                    .toLowerCase()
+                    .includes(search.toLowerCase());
+            }
+        });
+        return result;
     }
 </script>
 
