df076efcffe055444e58fbbf835f58dcf2bd6dfe
Evgeniy Kiryanov <evgeniy.kiryanov@roonyx.team>
2019-05-29 00:21:27 +0300

    dump

--- src/main/kotlin/com/kiryanov/database/controllers/DataBaseController.kt
+++ src/main/kotlin/com/kiryanov/database/controllers/DataBaseController.kt
@@ -8,6 +8,7 @@ import java.io.BufferedReader
 import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
+import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
 import javax.servlet.http.HttpServletResponse
@@ -83,15 +84,16 @@ class DataBaseController {
                 process.destroy()
 
                 println("===> Success on $type process.")
-                File(backupFileName)
+                File(backupFilePath.absolutePath + File.separator + backupFileName)
             } catch (ex: IOException) {
                 println("Exception: $ex")
                 throw RestException(ex.message)
             } catch (ex: InterruptedException) {
                 println("Exception: $ex")
                 throw RestException(ex.message)
+            } catch (ex: Exception) {
+                throw RestException(ex.message)
             }
-
         } else {
             throw RestException("Error: Invalid params.")
         }
