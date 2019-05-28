4d2d0e3ac0bf336ff7afd470b4b6ab52d1eded69
Evgeniy Kiryanov <evgeniy.kiryanov@roonyx.team>
2019-05-29 01:18:24 +0300

    dump

--- src/main/kotlin/com/kiryanov/database/controllers/DataBaseController.kt
+++ src/main/kotlin/com/kiryanov/database/controllers/DataBaseController.kt
@@ -1,15 +1,17 @@
 package com.kiryanov.database.controllers
 
 import org.apache.tomcat.util.http.fileupload.IOUtils
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.multipart.MultipartFile
 import java.io.BufferedReader
 import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
-import java.lang.Exception
-import java.text.SimpleDateFormat
 import java.util.*
 import javax.servlet.http.HttpServletResponse
 
@@ -21,6 +23,7 @@ class DataBaseController {
     private val user = "uqpjfgcmmyytyv"
     private val database = "dbektvt94eq8il"
     private val password = "39013be08b25bb41026d92a39398a4eb94bb0276348e1522e79229cbd94b5586"
+    private val backupFilePath = File(System.getProperty("user.home") + File.separator + "backup.sql")
 
     /*@GetMapping("/test")
     @ResponseBody
@@ -34,36 +37,50 @@ class DataBaseController {
     }*/
 
     @GetMapping("/backup")
-    fun backup(response: HttpServletResponse) {
-        val file = execute(host, user, database, password,"backup")
+    fun backup(response: HttpServletResponse): String {
+        val file = execute(host, user, database, password,"backup", backupFilePath.absolutePath)
 
         response.contentType = "application/sql"
         response.setHeader("Content-disposition", "attachment; filename=${file.name}")
         IOUtils.copy(file.inputStream(), response.outputStream)
+        return "File Downloaded"
     }
 
     @GetMapping("/restore")
-    fun restore(): String {
-        execute(host, user, database, password,"restore")
+    fun getRestore(): ResponseEntity<String> {
+        val page = "<html>\n" +
+                "\n" +
+                "<body>\n" +
+                "<h1>Spring MVC file upload example</h1>\n" +
+                "\n" +
+                "<form method=\"POST\" action=\"/api/db/restore\" enctype=\"multipart/form-data\">\n" +
+                "    <input type=\"file\" name=\"file\" /><br/>\n" +
+                "    <input type=\"submit\" value=\"Submit\" />\n" +
+                "</form>\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>"
 
-        return "restore"
+        return ResponseEntity.ok(page)
+    }
+
+    @PostMapping("/restore")
+    fun postRestore(@RequestParam("file") file: MultipartFile): ResponseEntity<String> {
+        val fileName = "uploadedTempFile.sql"
+        val tempFile = File(fileName)
+        if (!tempFile.exists()) tempFile.createNewFile()
+        IOUtils.copy(file.inputStream, tempFile.outputStream())
+
+        execute(host, user, database, password,"restore", tempFile.absolutePath)
+
+        return ResponseEntity.ok("Ok")
     }
 
     private fun execute(host: String, user: String,
                         databaseName: String, databasePassword: String,
-                        type: String): File {
-        val backupFilePath = File(System.getProperty("user.home")
-                + File.separator + "backup_" + databaseName)
+                        type: String, filePath: String): File {
 
-        if (!backupFilePath.exists()) {
-            backupFilePath.mkdirs()
-        }
-
-        val sdf = SimpleDateFormat("yyyyMMdd")
-//        String backupFileName = "backup_" + databaseName + "_" + sdf.format(new Date()) + ".sql";
-        val backupFileName = "backup_" + databaseName + "_" + "1" + ".sql"
-
-        val commands = getPgCommands(host, user, databaseName, backupFilePath, backupFileName, type)
+        val commands = getPgCommands(host, user, databaseName, filePath, type)
         return if (commands.isNotEmpty()) {
             try {
                 val pb = ProcessBuilder(commands)
@@ -84,7 +101,7 @@ class DataBaseController {
                 process.destroy()
 
                 println("===> Success on $type process.")
-                File(backupFilePath.absolutePath + File.separator + backupFileName)
+                File(filePath)
             } catch (ex: IOException) {
                 println("Exception: $ex")
                 throw RestException(ex.message)
@@ -100,8 +117,8 @@ class DataBaseController {
     }
 
     private fun getPgCommands(host: String, user: String,
-                              databaseName: String, backupFilePath: File,
-                              backupFileName: String, type: String): List<String> {
+                              databaseName: String, backupFilePath: String,
+                              type: String): List<String> {
 
         val commands = ArrayList<String>()
         when (type) {
@@ -118,7 +135,7 @@ class DataBaseController {
                 commands.add("-b") //include large objects in dump
                 commands.add("-v") //verbose mode
                 commands.add("-f") //output file or directory name
-                commands.add(backupFilePath.absolutePath + File.separator + backupFileName)
+                commands.add(backupFilePath)
                 commands.add("-d") //database name
                 commands.add(databaseName)
             }
@@ -133,7 +150,7 @@ class DataBaseController {
                 commands.add("-d")
                 commands.add(databaseName)
                 commands.add("-v")
-                commands.add(backupFilePath.absolutePath + File.separator + backupFileName)
+                commands.add(backupFilePath)
             }
             else -> return ArrayList()
         }
